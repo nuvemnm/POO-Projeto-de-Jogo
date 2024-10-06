3721728379485f62923a39659609609a3eddb040
nuvemnm <matheuscarv010@gmail.com>
2024-10-06 17:05:37 -0300

    setup display do jogo

--- code/main.py
+++ code/main.py
@@ -1,1 +1,31 @@
 from config import *
+
+class Jogo:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Mage Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):  
+        while self.running:
+            dt = self.clock.tick() / 1000
+
+            #pygame.QUIT signifa clicar no X da tela
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            #atualiza a tela trocando a atual pela gerada em segundo plano
+            pygame.display.update()
+        
+        
+        
+        
+        
+        pygame.quit()
+
+
+jogo = Jogo()
+jogo.run()
