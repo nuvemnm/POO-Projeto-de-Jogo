b66491605b36d8cedf78802ab4af12bb4a593a27
nuvemnm <matheuscarv010@gmail.com>
2024-10-07 15:50:23 -0300

    adicionado menu e movimentação ao personagem

--- code/main.py
+++ code/main.py
@@ -8,15 +8,33 @@ class Jogo:
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Mage Survivor')
         self.clock = pygame.time.Clock()
-        self.running = True
+        self.menu = True
+        self.running = False
 
         #grupo
         self.all_sprites = pygame.sprite.Group()
 
         #sprites
-        self.player = Jogador((400, 300), self.all_sprites)
+        self.player = Jogador((620, 360), self.all_sprites)
 
     def run(self):  
+        while self.menu:
+            dt = self.clock.tick(60) / 1000
+            keys = pygame.key.get_pressed()
+
+            #pygame.QUIT signifa clicar no X da tela
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.menu = False
+                elif keys[pygame.K_RETURN]:
+                    self.menu = False
+                    self.running = True
+
+            #desenha e atualiza o jogo
+            self.screen.fill('purple')
+            pygame.display.update()
+       
+        
         while self.running:
             dt = self.clock.tick(60) / 1000
 
