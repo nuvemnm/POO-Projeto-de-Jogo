3aca9baf073d9b0ab5d3608ccf10aa88e5bdefff
Naomy <naomyigb@gmail.com>
2024-10-22 20:05:59 -0300

    começando classe magia

---
+++ code/magia.py
@@ -0,0 +1,6 @@
+class Magia:
+    def __init__(self,nome:str,dano_base:int,dano_por_nivel:int,cooldown:int):
+        self.nome= nome
+        self.dano_base=dano_base
+        self.dano_por_nivel= dano_por_nivel
+        self.cooldown= cooldown
